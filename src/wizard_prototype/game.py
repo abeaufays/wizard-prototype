9feab45f67f055a154a48335b301d390ac982cc6
Alexandre Beaufays <alexandre.beaufays@kraken.tech>
2026-02-01 18:21:30 +0100

    Add ways to turn around without triggering a new turn

--- src/wizard_prototype/game.py
+++ src/wizard_prototype/game.py
@@ -18,11 +18,32 @@ class GameState:
 
     def update(self, cmd: str):
         match cmd:
+            # Moves
             case "h":
                 self.player_position = utils.add(self.player_position, LEFT)
+                self.player_direction = LEFT
+                self.new_turn()
             case "j":
                 self.player_position = utils.add(self.player_position, DOWN)
+                self.player_direction = DOWN
+                self.new_turn()
             case "k":
                 self.player_position = utils.add(self.player_position, UP)
+                self.player_direction = UP
+                self.new_turn()
             case "l":
                 self.player_position = utils.add(self.player_position, RIGHT)
+                self.player_direction = RIGHT
+                self.new_turn()
+            # Turn around
+            case "H":
+                self.player_direction = LEFT
+            case "J":
+                self.player_direction = DOWN
+            case "K":
+                self.player_direction = UP
+            case "L":
+                self.player_direction = RIGHT
+
+    def new_turn(self):
+        pass
